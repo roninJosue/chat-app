2ee43802a1f0fd978fbc98c90fa518df22690636
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-08-27 13:19:58 -0600

    feat: Add app's state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
+import {useState} from "react";
+
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
-
 import Home from "./pages/home";
+
 import {GlobalStyles} from "./styles/GlobalStyles";
+import io from "socket.io-client";
+
+const socket = io.connect(process.env.REACT_APP_SOCKET_URL)
 
 function App() {
+  const [username, setUsername] = useState('')
+  const [room, setRoom] = useState('')
+
   return (
     <BrowserRouter>
       <GlobalStyles />
       <Routes>
         <Route
           path='/'
-          element={<Home />}
+          element={
+          <Home
+            username={username}
+            setUsername={setUsername}
+            room={room}
+            setRoom={setRoom}
+            socket={socket}
+          />
+        }
         />
       </Routes>
     </BrowserRouter>
