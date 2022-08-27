f1e9226f6b68838a724cab859c5c01ef9b4fb6f4
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-08-27 12:18:25 -0600

    feat: Import global styles from App component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import {
 } from "react-router-dom";
 
 import Home from "./pages/home";
+import {GlobalStyles} from "./styles/GlobalStyles";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route
+          path='/'
+          element={<Home />}
+        />
       </Routes>
     </BrowserRouter>
   );
