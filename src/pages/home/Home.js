8caa9e1cddb8a20d6195223540bf8373c6eefad7
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-08-27 13:20:35 -0600

    feat: Add events to the inputs and button

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,5 @@
+import {useNavigate} from "react-router-dom";
+
 import {
   ButtonSecondary,
   Container,
@@ -6,13 +8,29 @@ import {
   StyledSelect
 } from "./styles";
 
-const Home = () => {
-  return(
+const Home = ({username, setUsername, room, setRoom, socket}) => {
+  const navigate = useNavigate()
+
+  const joinRoom = () => {
+    if (room !== '' && username !== '') {
+      socket.emit('join_room', {username, room})
+    }
+
+    navigate('/chat', {replace: true})
+  }
+
+  return (
     <Container>
       <FormContainer>
         <h1>{`<>DevRooms</>`}</h1>
-        <StyledInput placeholder='Username...' />
-        <StyledSelect>
+        <StyledInput
+          placeholder='Username...'
+          onChange={e => setUsername(e.target.value)}
+        />
+
+        <StyledSelect
+          onChange={e => setRoom(e.target.value)}
+        >
           <option>-- Select Room --</option>
           <option value='javascript'>JavaScript</option>
           <option value='node'>Node</option>
@@ -20,7 +38,12 @@ const Home = () => {
           <option value='react'>React</option>
         </StyledSelect>
 
-        <ButtonSecondary fullWidth>Join Room</ButtonSecondary>
+        <ButtonSecondary
+          fullWidth
+          onClick={joinRoom}
+        >
+          Join Room
+        </ButtonSecondary>
       </FormContainer>
     </Container>
   )
