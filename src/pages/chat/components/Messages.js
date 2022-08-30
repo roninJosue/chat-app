b2ae8d113bc89fbcf586b5a1c50529f602fcc92c
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-08-29 22:09:34 -0600

    feat: Show last 100 messages to the user

--- src/pages/chat/components/Messages.js
+++ src/pages/chat/components/Messages.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {Message, MessagesColum, MessageWrapper} from "../chatStyles";
 
 const RECEIVE_MESSAGE = 'receive_message'
@@ -8,8 +8,15 @@ const formatDateFromTimestamp = timestamp => {
   return date.toLocaleDateString()
 }
 
+const sortMessagesByDate = messages => {
+  return messages.sort(
+    (a, b) => parseInt(a.__createdtime__) - parseInt(b.__createdtime__)
+  )
+}
+
 const Messages = ({socket}) => {
   const [messagesReceive, setMessagesReceive] = useState([])
+  const messageColumRef = useRef(null)
 
   useEffect(() => {
     if (socket) {
@@ -28,8 +35,21 @@ const Messages = ({socket}) => {
     return () => socket.off(RECEIVE_MESSAGE)
   }, [socket])
 
+  useEffect(() => {
+    socket.on('last_100_messages', last100Messages => {
+      const _last100Messages = JSON.parse(last100Messages)
+      const orderedLast100Messages = sortMessagesByDate(_last100Messages)
+      setMessagesReceive(state => [...orderedLast100Messages, ...state])
+    })
+  }, [socket])
+
+  useEffect(() => {
+    messageColumRef.current.scrollTop = messageColumRef.current.scrollHeight
+  }, [messagesReceive]);
+
+
   return (
-    <MessagesColum>
+    <MessagesColum ref={messageColumRef}>
       {messagesReceive.map((msg, i) => (
         <Message key={i}>
           <MessageWrapper>
