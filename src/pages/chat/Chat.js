548d02badd9dcaf5e8770576b8f3d939e9c10863
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-08-29 21:49:40 -0600

    feat: Use SendMessage component in Chat

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,10 +1,16 @@
 import {ChatContainer} from "./chatStyles";
 import Messages from "./components/Messages";
+import SendMessage from "./components/SendMessage";
 
 const Chat = ({socket, username, room}) => {
   return(
     <ChatContainer>
-      <Messages socket={socket} />
+        <Messages socket={socket} />
+        <SendMessage
+          room={room}
+          socket={socket}
+          username={username}
+        />
     </ChatContainer>
   )
 }
